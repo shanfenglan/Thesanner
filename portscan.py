4d555b2de1e579c74319b1873b1823e872394ea4
shanfenglan <49781523+shanfenglan@users.noreply.github.com>
2020-04-23 17:28:43 +0800

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -7,7 +7,7 @@ def port_s(ip,port):
 
 
     class receive_package(object):
-        def __init__(self, l_ip, d_ip, o_p_l, c_p_l, c_port=None):
+        def __init__(self, l_ip, d_ip, o_p_l, c_p_l, c_port=None, nameee = None):
             self.port = c_port
             self.open_port_list1 = o_p_l
             self.close_port_list1 = c_p_l
@@ -15,29 +15,35 @@ def port_s(ip,port):
             self.destination_ip1 = d_ip
             self.all_port = []
             self.all_status = []
+            self.namee = nameee
 
         def prn(self, pkt):
             flags = pkt.sprintf("%TCP.flags%")
-            final_port = pkt.sprintf("%IP.sport%")
+            final_port = pkt.sprintf("%IP.src%:%IP.sport%")
+            pppport = pkt.sprintf("%IP.sport%")
             if flags == 'SA':
                 # print(pkt.sprintf("[+]%IP.src%:%IP.sport% opend"))
                 self.open_port_list1.append(final_port)
             else:
                 # print(pkt.sprintf("[-]%IP.src%:%IP.sport% closed"))
                 self.close_port_list1.append(final_port)
+            # self.port.remove(pppport)
 
         def run(self):
+            tmo = len(self.destination_ip1) * (1/15) * len(self.port)
+
+            # print(tmo)
             sniff(
-                lfilter=lambda x: x.haslayer(TCP) and x[IP].dst == self.local_ip1 and x[IP].src == self.destination_ip1 and x[
+                lfilter=lambda x: x.haslayer(TCP) and x[IP].dst == self.local_ip1 and x[IP].src in self.destination_ip1 and x[
                     TCP].flags == 'SA'
                                   or
-                                  x.haslayer(TCP) and x[IP].dst == self.local_ip1 and x[IP].src == self.destination_ip1 and x[
+                                  x.haslayer(TCP) and x[IP].dst == self.local_ip1 and x[IP].src in self.destination_ip1 and x[
                                       TCP].flags == 'RA'
-                , prn=self.prn, timeout=10)
+                , prn=self.prn, timeout=tmo)
 
         def output(self):
 
-            print('[+]The follow is "Port Scan" report for \'{0}\' :'.format(self.destination_ip1))
+            print('[+]The follow is "Port Scan" report for \'{0}\' :'.format(self.namee))
 
             print('[+]The opend port:')
             if not self.open_port_list1:
@@ -55,22 +61,22 @@ def port_s(ip,port):
                 self.all_status.append('closed')
 
 
-            # print(self.open_port_list1)
-            for i in self.open_port_list1:
-                if i in self.port:
-                    self.port.remove(i)
-            # print(port)
-            for i in self.close_port_list1:
-                if i in self.port:
-                    self.port.remove(i)
-            print('[+]The filtered port:')
-            if not self.port:
-                print('None')
-            # print(port)
-            for p in self.port:
-                print('--->'+str(p) + ' filtered')
-                self.all_port.append(p)
-                self.all_status.append('filtered')
+            # # print(self.open_port_list1)
+            # for i in self.open_port_list1:
+            #     if i in self.port:
+            #         self.port.remove(i)
+            # # print(port)
+            # for i in self.close_port_list1:
+            #     if i in self.port:
+            #         self.port.remove(i)
+            # print('[+]The filtered port:')
+            # if not self.port:
+            #     print('None')
+            # # print(port)
+            # for p in self.port:
+            #     print('--->'+str(p) + ' filtered')
+            #     self.all_port.append(p)
+            #     self.all_status.append('filtered')
 
     def get_host_ip():
         try:
@@ -88,6 +94,8 @@ def port_s(ip,port):
     final_state = []
 
     _ip = ip
+    # print(_ip)
+    # print(type(_ip))
     port1 = []
 
 
@@ -119,19 +127,21 @@ def port_s(ip,port):
         port2.append(str(i))
 
     for i in _ip:   # data processing
-        ipArray.extend([str(i) for i in  netaddr.IPNetwork(i)])
-
+        ipArray.extend([str(i) for i in netaddr.IPNetwork(i)])
+    # print(ipArray)
     for i in ipArray:   # send package
         for j in portArray:
             send(IP(dst=i)/TCP(dport=j, flags=2), verbose=False)
 
-    for i in _ip:
-        demo = receive_package(l_ip=local_ip, d_ip=i, o_p_l=open_port_list, c_p_l=close_port_list, c_port=port2)
-        demo.run()
-        demo.output()
-        final_port.extend(demo.all_port)
-        final_state.extend(demo.all_status)
+    demo = receive_package(l_ip=local_ip, d_ip=ipArray, o_p_l=open_port_list, c_p_l=close_port_list, c_port=port2,nameee = _ip)
+    demo.run()
+    demo.output()
+    final_port.extend(demo.all_port)
+    final_state.extend(demo.all_status)
+    _ip = '$'+str(_ip)
+    _ip = _ip.replace('/','\\')
+    file_operation.file_operate(_ip,final_port,final_state,'port')
 
-    file_operation.file_operate(ip,final_port,final_state,'port')
-
-# port_s(['39.156.66.14'],['1-80'])
+# port_s(['39.156.66.14/24'],['80'])
+# port_s(['39.156.66.14','39.156.66.14','39.156.66.14','39.156.66.14','39.156.66.14','39.156.66.14','39.156.66.14'],['80'])
+# port_s(['32.156.66.14'],['80'])
