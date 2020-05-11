c4a834781dd9c7ecb94b5a5529f0f36b3ddb7b3b
shanfenglan <839735789@qq.com>
2020-05-11 19:24:07 +0800

    Update Thescanner.py

--- Thescanner.py
+++ Thescanner.py
@@ -2,44 +2,16 @@
 # -*- coding: utf-8 -*-
 # Created by shanfenglan on 2020/2/8
 
-import argparse,subdomain,sys,os,re,dir_scan,portscan,Timer,bruteforce,service_scan,random,_banner
-
-def banner():
-    key = random.randint(1, 9)
-    keyy = int('3'+str(key))
-
-    if key in range(0,7):
-        print("""
-    \033[1;{0};40m
-    
-     _____ _          ____
-    |_   _| |__   ___/ ___|  ___ __ _ _ __  _ __   ___ _ __
-      | | | '_ \ / _ \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
-      | | | | | |  __/___) | (_| (_| | | | | | | |  __/ |
-      |_| |_| |_|\___|____/ \___\__,_|_| |_|_| |_|\___|_|
-    
-                    # Coded By Shanfenglan
-    \033[0m   """.format(keyy))
-    else:
-        print("""
-         ___________________________________ 
-    < Wow,Lucky dog!  Welcome to use TheScannercowsay!! >
-     ----------------------------------- 
-        \
-         \
-                                       .::!!!!!!!:.
-      .!!!!!:.                        .:!!!!!!!!!!!!
-      ~~~~!!!!!!.                 .:!!!!!!!!!UWWW$$$ 
-          :$$NWX!!:           .:!!!!!!XUWW$$$$$$$$$P 
-          $$$$$##WX!:      .<!!!!UW$$$$"  $$$$$$$$#       \033[1;32;40m<<<<<Coded By Shanfenglan>>>>>\033[0m
-          $$$$$  $$$UX   :!!UW$$$$$$$$$   4$$$$$* 
-          ^$$$B  $$$$\     $$$$$$$$$$$$   d$$R" 
-            \"*$bd$$$$      \'*$$$$$$$$$$$o+#\" 
-                 \"\"\"\"          """"""" 
-                    
-        """)
-    
-    print('\n\033[1;35;40m--------------------------------------------------------------------------------\033[0m')
+import Timer
+import _banner
+import argparse
+import bruteforce
+import dir_scan
+import os
+import portscan
+import service_scan
+import subdomain
+import sys
 
 
 def parser_error(errmsg):
@@ -53,15 +25,16 @@ def parse_args():
     _banner.banner()
     # parse the arguments
     parser = argparse.ArgumentParser(epilog='\tExample: \r\npython ' + sys.argv[0] + " -d google.com")
+    print("\033[1;36;40m[+]Remind:Only '-b' and '-dir' and '-ip  -s/-p' can use multiple threading.Multiple threading is useless for other features.\n\033[0m")
     parser.error = parser_error  # rewrite function
     parser._optionals.title = "OPTIONS"
-    parser.add_argument('-d', '--domain', help="Domain name to enumerate it's subdomains,exp:Thescanner.py -d www.qq.com",default=None)
-    parser.add_argument('-b', '--bruteforce', help='Enable the  bruteforce module,exp: Thescanner.py -b baidu.com', default=False)
+    parser.add_argument('-d', '--domain', help="Domain name to enumerate it's subdomains,exp:Thescanner.py -d qq.com",default=None)
+    parser.add_argument('-b', '--bruteforce', help='Enable the  bruteforce module,exp: Thescanner.py -b baidu.com -t 30', default=False)
     parser.add_argument('-p', '--ports', help='Automated port scanning,exp:Thescanner.py -ip 120.79.88.157 -p 80 443 22', nargs='*', default=None)
-    parser.add_argument('-s', '--service', help='Automated service scanning,exp:Thescanner.py -ip 39.156.66.14 -s 80 ', nargs='*', default=None)
-    parser.add_argument('-dir', '--directory', help='Automated directory scanning,exp:Thescanner.py -dir http://www.qq.com -t 100', nargs='?', default=False)
-    parser.add_argument('-dict', '--dictionary', help='Dictionary path', nargs='?', default=None)
-    parser.add_argument('-ip', help='ip address,Separated by space,exp:Thescanner.py -ip 39.156.66.14 -s 80 ',nargs='*')
+    parser.add_argument('-s', '--service', help='Automated service scanning,exp:Thescanner.py -ip 39.156.66.14 -s 80', nargs='*', default=None)
+    parser.add_argument('-dir', '--directory', help='Automated directory scanning,exp:Thescanner.py -dir http://www.qq.com -t 100',  default=False)
+    parser.add_argument('-dict', '--dictionary', help='Dictionary path', default=None)
+    parser.add_argument('-ip', help='ip address,Separated by space', nargs='*')
     parser.add_argument('-t', '--threads', help='Number of threads to use for the specific feature named directory scan or brute', type=int, default=30)
     return parser.parse_args()
 
@@ -113,6 +86,32 @@ def main():
     port1 = value.ports
     is_directory = value.directory
     thread = value.threads
+
+    flag1 = 0
+    flag2 = 0
+    if domain is not None:
+        flag1 +=1
+    if brutee:
+        flag1 +=1
+    if is_directory:
+        flag1 +=1
+    if ip is not None:
+        flag1+=1
+        flag2+=10
+    # print(flag1)
+    # print(port1,service_port)
+    if port1 is not None:
+        flag2 +=1
+    if service_port is not None:
+        flag2 +=1
+
+    if flag1 >= 2:
+        print('\033[1;35;40m[!]Waring:About "-d" and "-b" and "-ip",Only one parameter can appear .\033[0m')
+        sys.exit()
+    if flag2 != 11 and flag2 >=10:
+        print('\033[1;35;40m[!]Waring:Option "-p" must be used with "-s/-p" .\033[0m')
+        sys.exit()
+
     if domain is None and ip is None and is_directory is False and brutee is False:
         print('\033[1;36;40m\n[-]Error!!! You must have \'-d\' or \'-ip\' or \'-dir\' or \'-b \' in your parameter.\n\033[0m')
         sys.exit()
